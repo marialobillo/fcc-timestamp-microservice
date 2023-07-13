058e91f28b817e1f67053347535427db9b434ed4
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-07-13 12:40:54 +0200

    chore: install and setup prettier and eslint

--- index.js
+++ index.js
@@ -17,52 +17,51 @@ app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
-    res.json({ greeting: "hello API" });
+  res.json({ greeting: "hello API" });
 });
 
 app.get("/api/:date", (req, res) => {
-    try {
-        const date = req.params.date;
-        const numbersRegex = /^[0-9]+$/;
-        const isUnitFormat = numbersRegex.test(date);
-        let unixDate;
-        let utcDate;
-        if(!isUnitFormat){
-          if(new Date(date).toUTCString() === "Invalid Date"){
-            return res.status(200).json({ error: "Invalid Date" });
-          }
-          unixDate = Date.parse(date)
-          utcDate = new Date(date).toUTCString()
-        } else {
-          unixDate = Number(date)
-          const actualDate = new Date(unixDate)
-          utcDate = actualDate.toUTCString()
-        }
-        res.status(200).json({  
-          unix: unixDate,
-          utc: utcDate 
-        });
-    } catch (error) {
-        res.status(200).json({ error: "Invalid Date" });
+  try {
+    const date = req.params.date;
+    const numbersRegex = /^[0-9]+$/;
+    const isUnitFormat = numbersRegex.test(date);
+    let unixDate;
+    let utcDate;
+    if (!isUnitFormat) {
+      if (new Date(date).toUTCString() === "Invalid Date") {
+        return res.status(200).json({ error: "Invalid Date" });
+      }
+      unixDate = Date.parse(date);
+      utcDate = new Date(date).toUTCString();
+    } else {
+      unixDate = Number(date);
+      const actualDate = new Date(unixDate);
+      utcDate = actualDate.toUTCString();
     }
+    res.status(200).json({
+      unix: unixDate,
+      utc: utcDate,
+    });
+  } catch (error) {
+    res.status(200).json({ error: "Invalid Date" });
+  }
 });
 
 app.get("/api", (req, res) => {
-    const date = new Date();
-    res.status(200).json({
-        unix: date.getTime(),
-        utc: date.toUTCString(),
-    });
-})
+  const date = new Date();
+  res.status(200).json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
 
 const port = process.env.PORT || 3000;
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-    console.log("Your app is listening on port " + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
-
